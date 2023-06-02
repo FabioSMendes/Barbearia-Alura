2435c198557f4d0ab0347785462347aeacd92f4e
Fabio Mendes <fabio.smendes28@gmail.com>
2023-06-02 14:01:07 -0300

    validando formulario com JS utilizando o focusout

--- js/validacao.js
+++ js/validacao.js
@@ -5,6 +5,12 @@ const bairro = document.querySelector('#bairro');
 const cidade = document.querySelector('#cidade');
 const estado = document.querySelector('#estado');
 const message = document.querySelector('#message');
+const myForm = document.querySelector('#myForm')
+const nomesobrenome = document.querySelector('#nomesobrenome')
+const email = document.querySelector('#email')
+const senha = document.querySelector('#senha')
+const telefone = document.querySelector('#telefone')
+const cpf = document.querySelector('#cpf')
 
 cep.addEventListener('focusout', async () => {
     try {
@@ -39,4 +45,51 @@ cep.addEventListener('focusout', async () => {
     }
 
 })
+
+//validação de dados
+
+myForm.addEventListener('focusout', (event) => {
+    event.preventDefault();
+
+    //verifica se o nome está fazio
+    if (nomesobrenome.value === '') {
+    alert("por favor, preencha seu nome");
+    return;
+}
+
+    // verifica se o nome está fazio e se é válido
+    if (email.value === '' || !isEmailValid(email.value)) {
+    alert("por favor, preencha seu email");
+    return;
+    }
+
+    if (!validatePassword(senha.value, 8)) {
+        alert ('A senha precisa ter entre 6 e 8 dígitos.');
+        return;
+    }
+
+});
+
+    // Função que valida e-mail
+function isEmailValid(email) {
+    //cria uma regex para validar email
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if(emailRegex.test(email)) {
+        return true;
+    }
+
+    return false;
+}
+
+function validatePassword(senha, minDigits) {
+    if(senha.lenghth>= minDigits) {
+        //senha válida
+        return true;
+    }
+
+    return false;
+}
 
